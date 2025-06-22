028860f337dbb72bf969723a9c0baf002d63b7ff
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-06-22 11:08:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
 
