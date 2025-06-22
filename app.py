83c06875e16543868ea6a704d58235cb1810cf2a
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-06-22 19:19:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,6 @@ def sw():
     return send_from_directory('static', 'service-worker.js')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=False, host='0.0.0.0', port=port)
