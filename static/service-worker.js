67cf1974aed3b579b7b2d01d285e61326488a08b
Melvin J Bonner <140230755+jb24000@users.noreply.github.com>
2025-06-22 05:14:56 -0400

    Add files via upload

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,21 +1,23 @@
+const CACHE_NAME = 'take-home-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/styles.css',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('takehomepay').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/static/styles.css',
-        '/static/icon-192.png',
-        '/static/icon-512.png'
-      ]);
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
